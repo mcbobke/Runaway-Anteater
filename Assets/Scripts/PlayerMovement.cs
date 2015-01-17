fca65935881472f4428a87fc297e21c6009a7bee
Matthew Christopher Bobke <mcbobke@gmail.com>
2015-01-17 12:35:11 -0800

    Removed unnecessary code

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -37,13 +37,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "background")
-        {
-            /*player.rigidbody2D.velocity = new Vector2(0, 0);
-            player.rigidbody2D.angularVelocity = 0;*/
-        }
-
-        else if (collision.gameObject.tag == "obstacle")
+        if (collision.gameObject.tag == "obstacle")
         {
             --collisionCount;
             Destroy(collision.gameObject);
