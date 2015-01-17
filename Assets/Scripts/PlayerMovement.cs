eb9f00cef98e0755dd8c2bd424570e45b53e2964
Matthew Christopher Bobke <mcbobke@gmail.com>
2015-01-16 22:03:56 -0800

    Initial upload
    Initial upload

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public GameObject player;
+    public float speed;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    Screen.SetResolution(800, 600, false);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+        MoveCharacter();
+	}
+
+    void MoveCharacter()
+    {
+        Vector2 currentPos = player.rigidbody2D.position;
+
+        if (Input.GetKey(KeyCode.RightArrow) && currentPos.x < 2.0f)
+        {
+            player.rigidbody2D.MovePosition(new Vector2(currentPos.x + speed, currentPos.y));
+        }
+
+        else if (Input.GetKey(KeyCode.LeftArrow) && currentPos.x > -2.0f)
+        {
+            player.rigidbody2D.MovePosition(new Vector2(currentPos.x - speed, currentPos.y));
+        }
+    }
+}
