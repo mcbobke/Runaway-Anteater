934f4d6d1414a0924913d9e406fa8521ccb4fb0c
Matthew Christopher Bobke <mcbobke@gmail.com>
2015-01-17 17:35:14 -0800

    Adding object spawner

---
+++ Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    public float speed;
+
+	void Update ()
+	{
+	    Scroll();
+	}
+
+    void Scroll()
+    {
+        Vector3 currentPos = transform.position;
+
+        if (currentPos.y <= -6.0f)
+        {
+            transform.position = new Vector3(0, 6);
+        }
+
+        else
+        {
+            transform.position = new Vector3(currentPos.x, currentPos.y - speed);
+        }
+    }
+}
