e4e3dc9d7eb1d524c8c1d65f718672be817bd25f
kgwong <wong.kevin47@gmail.com>
2015-01-17 18:53:03 -0800

    Added horizontal obstacles

--- Assets/Scripts/TestObstacle.cs
+++ Assets/Scripts/TestObstacle.cs
@@ -5,13 +5,14 @@ public class TestObstacle : MonoBehaviour
 {
 
     public GameObject obstacle;
-    public float vel;
+    public float X_vel;
+	public float Y_vel;
 	
 	// Update is called once per frame
 	void Update () 
     {
         Vector2 currentPos = obstacle.rigidbody2D.position;
-	    obstacle.rigidbody2D.MovePosition(new Vector2(currentPos.x, currentPos.y - vel));
+	    obstacle.rigidbody2D.MovePosition(new Vector2(currentPos.x + X_vel, currentPos.y + Y_vel));
 	}
 
     void OnCollisionEnter2D(Collision2D collision)
