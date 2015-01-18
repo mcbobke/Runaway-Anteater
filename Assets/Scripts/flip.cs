bdd574a1186fcf3e0cb80f91754bb78810efdcb8
Nina V <nvolkmut@uci.edu>
2015-01-18 12:48:56 -0800

    Added in the student animation

---
+++ Assets/Scripts/flip.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flip : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Vector3 scale = transform.localScale;
+		transform.localScale = new Vector3(scale.x * -1, 1, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
